d683452fd3bd2cb7b2cb8c63b29f4e01169c2c44
Artur Judycki <artur.judycki55@gmail.com>
2022-10-19 19:36:49 +0200

    zadanie7

--- app/polls/admin.py
+++ app/polls/admin.py
@@ -5,6 +5,7 @@ from .models import User, Osoba
 
 class OsobaAdmin(admin.ModelAdmin):
     readonly_fields = ('data_dodania',)
+    list_display = ['imie', 'nazwisko', 'miesiac_urodzenia', 'data_dodania']
 
 admin.site.register(User)
 admin.site.register(Osoba,OsobaAdmin)
