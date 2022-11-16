5e903f749a6dc98e2d6101902aa9799bb3168d17
Artur Judycki <artur.judycki55@gmail.com>
2022-11-16 13:41:19 +0100

    lab_6_zadanie_1

--- app/polls/urls.py
+++ app/polls/urls.py
@@ -1,17 +1,20 @@
-from django.urls import path
+from django.urls import path, include
 
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    # path('osobas/', views.osoba_list),
-    # path('osobas/<int:pk>/', views.osoba_detail),
-    # path('osobas/add/', views.osoba_add),
-    # path('osobas/<imie>/', views.osoba_detail_name),
-    path('osobas/', views.OsobaList.as_view()),
-    path('osobas/<int:pk>/', views.OsobaDetail.as_view()),
-    path('osobas/add/', views.OsobaAdd.as_view()),
-    path('osobas/<imie>/', views.OsobaDetailName.as_view()),
+    path('api-auth/', include('rest_framework.urls')),
+    path('osobas/', views.osoba_list),
+    path('osobas/<int:pk>/', views.osoba_detail),
+    path('osobas/update/<int:pk>/', views.osoba_update_delete),
+    path('osobas/delete/<int:pk>/', views.osoba_update_delete),
+    path('osobas/add/', views.osoba_add),
+    path('osobas/<imie>/', views.osoba_detail_name),
+    # path('osobas/', views.OsobaList.as_view()),
+    # path('osobas/<int:pk>/', views.OsobaDetail.as_view()),
+    # path('osobas/add/', views.OsobaAdd.as_view()),
+    # path('osobas/<imie>/', views.OsobaDetailName.as_view()),
     path('druzynas/', views.druzyna_list),
     path('druzynas/<int:pk>/', views.druzyna_detail),
     path('druzynas/add/', views.druzyna_add),
--- app/polls/views.py
+++ app/polls/views.py
@@ -3,7 +3,9 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from rest_framework import status
 from rest_framework import filters
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from .models import Osoba, Druzyna
 from .serializers import OsobaModelSerializer, DruzynaModelSerializer
@@ -13,56 +15,67 @@ from rest_framework.views import APIView
 
 from django_filters.rest_framework import DjangoFilterBackend
 
-# @api_view(['GET'])
-# def osoba_list(request):
-#     if request.method == 'GET':
-#         osobas = Osoba.objects.all()
-#         serializer = OsobaModelSerializer(osobas, many=True)
-#         return Response(serializer.data)
-#
-# @api_view(['GET', 'PUT', 'DELETE'])
-# def osoba_detail(request, pk):
-#
-#     try:
-#         osoba = Osoba.objects.get(pk=pk)
-#     except Osoba.DoesNotExist:
-#         return Response(status=status.HTTP_404_NOT_FOUND)
-#
-#     if request.method == 'GET':
-#         serializer = OsobaModelSerializer(osoba)
-#         return Response(serializer.data)
-#
-#     elif request.method == 'PUT':
-#         serializer = OsobaModelSerializer(osoba, data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-#
-#     elif request.method == 'DELETE':
-#         osoba.delete()
-#         return Response(status=status.HTTP_204_NO_CONTENT)
-#
-# @api_view(['POST'])
-# def osoba_add(request):
-#     if request.method == 'POST':
-#         serializer = OsobaModelSerializer(data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-#
-# @api_view(['GET'])
-# def osoba_detail_name(request, imie):
-#
-#     try:
-#         osobas = Osoba.objects.all().filter(imie=imie)
-#     except Osoba.DoesNotExist:
-#         return Response(status=status.HTTP_404_NOT_FOUND)
-#
-#     if request.method == 'GET':
-#         serializer = OsobaModelSerializer(osobas, many=True)
-#         return Response(serializer.data)
+@api_view(['GET'])
+def osoba_list(request):
+    if request.method == 'GET':
+        osobas = Osoba.objects.all()
+        serializer = OsobaModelSerializer(osobas, many=True)
+        return Response(serializer.data)
+
+@api_view(['GET'])
+def osoba_detail(request, pk):
+
+    try:
+        osoba = Osoba.objects.get(pk=pk)
+    except Osoba.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        osoba = Osoba.objects.get(pk=pk)
+        serializer = OsobaModelSerializer(osoba)
+        return Response(serializer.data)
+
+@api_view(['PUT', 'DELETE'])
+@authentication_classes([SessionAuthentication, BasicAuthentication])
+@permission_classes([IsAuthenticated])
+def osoba_update_delete(request, pk):
+
+    try:
+        osoba = Osoba.objects.get(pk=pk)
+    except Osoba.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'PUT':
+        serializer = OsobaModelSerializer(osoba, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        osoba.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+@api_view(['POST'])
+def osoba_add(request):
+    if request.method == 'POST':
+        serializer = OsobaModelSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET'])
+def osoba_detail_name(request, imie):
+
+    try:
+        osobas = Osoba.objects.all().filter(imie=imie)
+    except Osoba.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = OsobaModelSerializer(osobas, many=True)
+        return Response(serializer.data)
 
 @api_view(['GET'])
 def druzyna_list(request):
@@ -103,62 +116,62 @@ def druzyna_add(request):
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-class OsobaList(APIView):
-
-    def get(self, request, format=None):
-        osobas = Osoba.objects.all()
-        serializer = OsobaModelSerializer(osobas, many=True)
-        return Response(serializer.data)
-
-    def post(self, request, format=None):
-        serializer = OsobaModelSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-class OsobaDetail(APIView):
-
-    def get_object(self, pk):
-        try:
-            return Osoba.objects.get(pk=pk)
-        except Osoba.DoesNotExist:
-            raise Http404
-
-    def get(self, request, pk, format=None):
-        osoba = self.get_object(pk)
-        serializer = OsobaModelSerializer(osoba)
-        return Response(serializer.data)
-
-    def put(self, request, pk, format=None):
-        osoba = self.get_object(pk)
-        serializer = OsobaModelSerializer(osoba, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk, format=None):
-        osoba = self.get_object(pk)
-        osoba.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-
-
-class OsobaAdd(APIView):
-
-    def post(self, request, format=None):
-        serializer = OsobaModelSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-class OsobaDetailName(APIView):
-
-    def get(self, request, imie, format=None):
-        osobas = Osoba.objects.all().filter(imie=imie)
-        serializer = OsobaModelSerializer(osobas, many=True)
-        return Response(serializer.data)
+# class OsobaList(APIView):
+#
+#     def get(self, request, format=None):
+#         osobas = Osoba.objects.all()
+#         serializer = OsobaModelSerializer(osobas, many=True)
+#         return Response(serializer.data)
+#
+#     def post(self, request, format=None):
+#         serializer = OsobaModelSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#
+# class OsobaDetail(APIView):
+#
+#     def get_object(self, pk):
+#         try:
+#             return Osoba.objects.get(pk=pk)
+#         except Osoba.DoesNotExist:
+#             raise Http404
+#
+#     def get(self, request, pk, format=None):
+#         osoba = self.get_object(pk)
+#         serializer = OsobaModelSerializer(osoba)
+#         return Response(serializer.data)
+#
+#     def put(self, request, pk, format=None):
+#         osoba = self.get_object(pk)
+#         serializer = OsobaModelSerializer(osoba, data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#
+#     def delete(self, request, pk, format=None):
+#         osoba = self.get_object(pk)
+#         osoba.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
+#
+#
+# class OsobaAdd(APIView):
+#
+#     def post(self, request, format=None):
+#         serializer = OsobaModelSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#
+# class OsobaDetailName(APIView):
+#
+#     def get(self, request, imie, format=None):
+#         osobas = Osoba.objects.all().filter(imie=imie)
+#         serializer = OsobaModelSerializer(osobas, many=True)
+#         return Response(serializer.data)
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
