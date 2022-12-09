a71a87b7cbaaf374c06de18b0f591a4301803905
Artur Judycki <artur.judycki55@gmail.com>
2022-12-09 17:17:11 +0100

    lab_08_zadanie_5

--- app/polls/tests/test_views.py
+++ app/polls/tests/test_views.py
@@ -1,4 +1,8 @@
 from django.test import TestCase, Client
+from rest_framework import status
+from rest_framework.test import APITestCase
+from rest_framework.test import APIClient
+from rest_framework.authtoken.models import Token
 
 from ..models import Osoba
 
@@ -10,3 +14,23 @@ class OsobaListViewTest(TestCase):
     def test_view_url_exists_at_desired_location(self):
         response = self.client.get('/polls/osobas/')
         self.assertEqual(response.status_code, 403)
+
+class OsobaTests(APITestCase):
+
+    def test_create_osoba(self):
+        self.client = APIClient()
+        self.client.login(username='artur', password='123')
+        url = '/polls/osobas/add/'
+        data = {'imie': 'Ala', 'nazwisko': 'Kot', 'miesiac_urodzenia': 5}
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Osoba.objects.count(), 1)
+        self.assertEqual(Osoba.objects.get().name, 'Ala')
+
+    def test_view_osoba(self):
+        token = Token.objects.get(user__username='artur')
+        self.client = APIClient()
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + token.key)
+        url = '/polls/osobas/1/'
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
