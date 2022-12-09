3d7651d8f90dc6d2dd5473b850b7a7c2a676b3c6
Artur Judycki <artur.judycki55@gmail.com>
2022-12-09 14:07:56 +0100

    lab_08_zadanie_2

--- app/polls/tests/test_models.py
+++ app/polls/tests/test_models.py
@@ -1,12 +1,13 @@
 from django.test import TestCase
 
-from ..models import Osoba
+from ..models import Osoba, Druzyna
 
 
 class OsobaModelTest(TestCase):
     @classmethod
     def setUpTestData(cls):
         Osoba.objects.create(imie='Juraj', nazwisko='Janosik')
+        Osoba.objects.create(imie='Sherlock', nazwisko='Holmes')
 
     def test_first_name_label(self):
         osoba = Osoba.objects.get(id=1)
@@ -17,3 +18,19 @@ class OsobaModelTest(TestCase):
         osoba = Osoba.objects.get(id=1)
         max_length = osoba._meta.get_field('imie').max_length
         self.assertEqual(max_length, 45)
+
+    def test_osoba_different_id(self):
+        juraj = Osoba.objects.get(imie="Juraj")
+        sherlock = Osoba.objects.get(imie="Sherlock")
+        self.assertEqual(sherlock.id - juraj.id, 1)
+
+class DruzynaModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        Druzyna.objects.create(nazwa='IMDB FC', kraj='UK')
+        Druzyna.objects.create(nazwa='Filmweb FC', kraj='PL')
+
+    def test_druzyna_different_id(self):
+        imdb = Druzyna.objects.get(kraj="UK")
+        filmweb = Druzyna.objects.get(kraj="PL")
+        self.assertEqual(filmweb.id - imdb.id, 2)
